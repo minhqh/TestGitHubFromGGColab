674941b2ec0ff800055fded31c6d76c924f2782f
minhqh <minh.hoquang.dev@gmail.com>
2025-08-03 12:34:39 +0700

    Add picture and change function

--- test.py
+++ test.py
@@ -3,12 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 image = cv2.imread("Tsumiki.png")
+image2 = cv2.imread("Tsumiki2.png")
 
 def show_image(image , title):
-  image = cv2.cvtColor(image , cv2.COLOR_RGB2BGR)
+  image = cv2.cvtColor(image , cv2.COLOR_RGB2HLS)
   plt.imshow(image)
   plt.title(title)
   plt.axis("off")
   plt.show()
 
 show_image(image , "Tsumiki.chan")
+show_image(image2 , "Tsumiki2")
